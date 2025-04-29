4b4c9b5780f215c88350d5aeee8fe5bb149fe440
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-04-29 23:15:11 +0530

    Add files via upload

---
+++ Week4/Node/index.mjs
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello World'));
+console.log(chalk.red.bold('This is error message'));
+console.log(chalk.green.underline('This is success message'))
+console.log(chalk.blue.bgRed.bold('Sup!?'));
