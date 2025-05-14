487fbd0affbb10e1bcd12b2abf37dff3630e5a48
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-05-14 23:20:49 +0530

    Add files via upload

---
+++ Week5/5.2/middlewares/index.js
@@ -0,0 +1,14 @@
+
+app.use(function(req, res, next) {
+    console.log("request received");
+    next();
+})
+
+app.get("/sum", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
