9a7523c2939127cd5d418e4a3825f106286c674e
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-06-13 23:10:38 +0530

    Add files via upload

---
+++ week8/MongoDeepDive/courseselling_without_JWT/middleware/user.js
@@ -0,0 +1,19 @@
+const {User} = require("../db")
+
+function userMiddleware(req,res,next){
+    const username = req.headers.username;
+    const password = req.headers.password;
+    User.findOne({
+        username : username,
+        password : password
+    })
+    .then(function(value){
+        if (value){
+            next();
+        }else{
+            res.status(403).send("Invalid username or password");
+        }
+    })
+}
+
+module.exports = userMiddleware;
