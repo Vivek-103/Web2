27e6174bd6dbb0adc75d31fffd483f66814018d6
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-07-08 22:46:56 +0530

    Update Types.ts

--- Typescript/02Basics/Types.ts
+++ Typescript/02Basics/Types.ts
@@ -1,3 +1,7 @@
+//We’ve been using object types and union types by writing them directly in type annotations. 
+//This is convenient, but it’s common to want to use the same type more than once and refer to it by a single name.
+//A type alias is exactly that - a name for any type.
+
 type User = {
     name : string;
     email : string;
