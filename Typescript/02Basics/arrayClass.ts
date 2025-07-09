4a81471a72d3039eb7bb96fc5a2b14e0eade33ab
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-07-09 16:10:06 +0530

    Create arrayClass.ts

---
+++ Typescript/02Basics/arrayClass.ts
@@ -0,0 +1,20 @@
+const superHeros :string[] = []
+// const heroPower: number[] = []
+const heroPower : Array<number> = []// another way to define an array
+
+type User1 = {
+    name :string
+    isActive:boolean
+}
+
+const allUsers : User1[] = []
+
+const MLModels : number[][] = [
+    [255,255,255],
+    []
+]
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name : " ",isActive:true})
